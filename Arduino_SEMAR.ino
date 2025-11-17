b9b9926f011be0355c95b680f32c1240932b5b0d
Muhammad Zaki Azhar <149247787+muzazr@users.noreply.github.com>
2025-11-17 16:59:40 +0700

    First commit

---
+++ Arduino_SEMAR.ino
@@ -0,0 +1,84 @@
+#define BLYNK_TEMPLATE_ID "TMPL6YZS6x4FW"
+#define BLYNK_TEMPLATE_NAME "PREMOS"
+#define BLYNK_AUTH_TOKEN "Z2rxBeMHjztCVC3ijJyuSuF8meTTr69y"
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp8266.h>
+
+const int pulsePin = A0; // Pin untuk pulse sensor
+const float refVolt = 5.0;
+int pulseValue;
+int pulseBpm, bloodGlucose, cholesterol, asamUratPria, asamUratWanita;
+
+char auth[] = "Z2rxBeMHjztCVC3ijJyuSuF8meTTr69y";
+char ssid[] = "muzazz";
+char pass[] = "9876543210";
+BlynkTimer timer;
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  Blynk.begin(auth, ssid, pass);
+  while (!Blynk.connected()) {
+    Serial.println("Connecting to Blynk...");
+    delay(1000);
+  }
+  Serial.println("Connected to Blynk");
+
+  pinMode(pulsePin, INPUT);
+}
+
+void loop() {
+  pulseValue = analogRead(pulsePin);
+  pulseBpm = 12000 / pulseValue;
+
+  Serial.println(pulseValue);
+  bloodGlucose = (pulseBpm * 140) / 120;
+  cholesterol = (pulseBpm * 230) / 120;
+  asamUratPria = (pulseBpm * 10) / 120;
+  asamUratWanita = (pulseBpm * 8) / 120;
+
+
+    Serial.print("Heart rate: ");
+    Serial.print(pulseBpm);
+    Serial.println(" BPM");
+
+    Serial.print("Blood Glucose: ");
+    Serial.print(bloodGlucose);
+    Serial.println(" mg/dl");
+
+    Serial.print("Cholesterol: ");
+    Serial.print(cholesterol);
+    Serial.println(" mg/dl");
+
+    Serial.print("Asam Urat Pria: ");
+    Serial.print(asamUratPria);
+    Serial.println(" mg/dl");
+
+    Serial.print("Asam Urat Wanita: ");
+    Serial.print(asamUratWanita);
+    Serial.println(" mg/dl");
+
+    Blynk.virtualWrite(V0, pulseBpm);
+    Blynk.virtualWrite(V1, bloodGlucose);
+    Blynk.virtualWrite(V2, cholesterol);
+    Blynk.virtualWrite(V3, asamUratPria);
+    Blynk.virtualWrite(V4, asamUratWanita);
+
+
+    // String messagePulse = "Heart rate from Pulse: " + String(pulseBpm) + " BPM";
+    // String glucose = "Blood glucose from Pulse: " + String((pulseBpm * 140) / 120) + " mg/dl";
+    // Serial.println(messagePulse);
+    delay(1000);
+
+  Blynk.run();
+}
